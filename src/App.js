9f39295d6a573f36489235e799d24cd598dbfb77
yankareddy <reddy.a111freind@gmail.com>
2024-03-11 04:47:38 +0000

    reuseable-banner

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css'
+
+import BannerCardsItem from './components/BannerCardItem'
+
+const bannerCardsList = [
+  {
+    id: 1,
+    headerText: 'The Seasons Latest',
+    description: 'Get the seasons all latest designs in a flick of your hand',
+    className: 'card-1',
+  },
+  {
+    id: 2,
+    headerText: 'Our New Designs',
+    description:
+      'Get the designs developed by our in-house team all for yourself',
+    className: 'card-2',
+  },
+  {
+    id: 3,
+    headerText: 'Insiders',
+    description: 'Get the top class products for yourself with an extra off',
+    className: 'card-3',
+  },
+]
+
+const App = () => (
+  <div className="app-container">
+    <ul className="banner-card-list">
+      {bannerCardsList.map(eachBanner => (
+        <BannerCardsItem bannerDetails={eachBanner} key={eachBanner.id} />
+      ))}
+    </ul>
+  </div>
+)
+
+export default App
